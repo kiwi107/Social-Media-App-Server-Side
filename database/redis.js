1ed793879897ebc26055b12ea35fe24308d4be63
karim sherif <119266066+kiwi107@users.noreply.github.com>
2024-02-17 21:59:07 +0200

    redis update

--- database/redis.js
+++ database/redis.js
@@ -1,4 +1,9 @@
 const Redis = require('ioredis');
-const redisClient = new Redis();
+
+// Internal Redis URL provided by Render
+const redisURL = 'redis://red-cn8gu5v109ks739osqv0:6379';
+
+// Create a new Redis client instance with the provided URL
+const redisClient = new Redis(redisURL);
 
 module.exports = redisClient;
